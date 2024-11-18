14d274d56da1c83c76aa24ae8c1a7e752a1ad6c1
Marina Dvoretskova <smallestproblem@gmail.com>
2024-11-18 16:56:13 +0300

    initial comit

---
+++ src/main/java/ru/yanmayak/t1openschool/T1openschoolApplication.java
@@ -0,0 +1,13 @@
+package ru.yanmayak.t1openschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1openschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1openschoolApplication.class, args);
+    }
+
+}
